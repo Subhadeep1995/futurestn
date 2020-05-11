da7811c8e0b3747e477fd16e3dc6be2233c55d2d
Subhadeep <subhadeep.code@gmail.com>
2020-05-11 11:33:28 +0530

    added plugins for contentful and images

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,7 +12,7 @@ const contact = () => {
                     <p><input class={contactStyles.contact_input} type="hidden" name="form-name" value="contact" /></p>
                     <p><input class={contactStyles.contact_input} type="text" name="name" placeholder="Your Full Name"/></p>
                     <p><input class={contactStyles.contact_input} type="email" name="email" placeholder="Your Email Address"/></p>
-                    <p><textarea class={contactStyles.contact_input} name="" id="" cols="30" rows="10"></textarea></p>
+                    <p><textarea class={contactStyles.contact_input} name="message" id="message" cols="30" rows="10"></textarea></p>
                     <p><input class={contactStyles.contact_input} type="submit" value="Send"/></p>
                 </form>
             </div>
